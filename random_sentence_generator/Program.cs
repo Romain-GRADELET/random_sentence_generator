bc2d0fb84f13d747f26fb01828634647ac2ad3a8
Romain-GRADELET <romain.gradelet73@outlook.fr>
2024-01-11 15:53:14 +0100

    Obtenir élément aléatoire

--- random_sentence_generator/Program.cs
+++ random_sentence_generator/Program.cs
@@ -1,2 +1,69 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace random_sentence_generator
+{
+    class Program
+    {
+        static string ObtenirElementAleatoire(string[] t) 
+        {
+            Random r = new Random();
+            int index = r.Next(t.Length);
+            return t[index];
+        }
+
+        static void Main(string[] args)
+        {
+            // Généréer des phrase sujet -> verbe -> complément
+            var sujets = new string[]
+            {
+                "Un lapin",
+                "Un homme",
+                "Une femme",
+                "Un chat",
+                "Un chien",
+                "Une tortue",
+                "Un enfant",
+                "Un magicien",
+                "un cheval",
+            };
+
+            var verbes = new string[]
+            {
+                "mange",
+                "écrase",
+                "regarde",
+                "néttoie",
+                "avale",
+                "attrape",
+                "marche",
+                "détruit",
+                "s'accroche à"
+            };
+
+            var complements = new string[]
+            {
+                "un arbre",
+                "un livre",
+                "la lune",
+                "le soleil",
+                "le ciel",
+                "une piscine",
+                "un gateau",
+                "un crapaud",
+                "une carte",
+            };
+
+            for (int i = 0; i < 10; i++) 
+            {
+                string sujet = ObtenirElementAleatoire(sujets);
+                string verbe = ObtenirElementAleatoire(verbes);
+                string complement = ObtenirElementAleatoire(complements);
+
+                Console.WriteLine(sujet + " " + verbe + " " + complement);
+            }
+
+           
+
+        }
+    }
+}
